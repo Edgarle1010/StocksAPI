c5ab8f7bfa380f7264d64c4b6a65605be70abdef
Edgar Francisco López Enríquez <109615404+elopezLumston@users.noreply.github.com>
2023-05-23 12:18:18 -0600

    [FIX] CharRange cases

--- Sources/StocksAPI/Models/ChartRange.swift
+++ Sources/StocksAPI/Models/ChartRange.swift
@@ -15,7 +15,7 @@ public enum ChartRange: String, CaseIterable {
     case sixMonth = "6mo"
     case ytd
     case oneYear = "1y"
-    case towYear = "2y"
+    case twoYear = "2y"
     case fiveYear = "5y"
     case tenYear = "10y"
     case max
@@ -25,7 +25,7 @@ public enum ChartRange: String, CaseIterable {
         case .oneDay: return "1m"
         case .oneWeek: return "5m"
         case .oneMonth: return "90m"
-        case .threeMonth, .sixMonth, .ytd, .oneYear, .towYear: return "1d"
+        case .threeMonth, .sixMonth, .ytd, .oneYear, .twoYear: return "1d"
         case .fiveYear, .tenYear: return "1wk"
         case .max: return "3mo"
         }
